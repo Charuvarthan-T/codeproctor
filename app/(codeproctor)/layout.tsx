266f57cf4c7c84ba820e2b591794bf8fc0d49f1c
renny <rennydan72@gmail.com>
2025-09-02 21:07:34 +0530

    fix: authorised entry only

--- app/(codeproctor)/layout.tsx
+++ app/(codeproctor)/layout.tsx
@@ -1,22 +1,28 @@
+"use client";
 import { ReactNode } from "react";
 import AppSidebar from "@/components/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppHeader } from "@/components/app-header";
+import { useSession } from "next-auth/react";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export default function CodeProctorLayout({ children }: LayoutProps) {
-  return (
+  const { data: session } = useSession();
+
+  return session?.user.role === "admin" ? (
     <SidebarProvider>
       <AppSidebar />
       <div className="flex flex-1 flex-col">
         <AppHeader />
-        <main className="flex flex-1 flex-col p-2">
-          {children}
-        </main>
+        <main className="flex flex-1 flex-col p-2">{children}</main>
       </div>
     </SidebarProvider>
+  ) : (
+    <div>
+      <h1>Unauthorised Access. Prohibited entry into site.</h1>
+    </div>
   );
 }
